64429d90294339471a7f170192b70b5b247cd4f0
inadeqtfuturs <alexj.christie@gmail.com>
2023-10-14 12:13:24 -0500

    feat(post): update markup. add tags to post pages

--- src/components/PostPage/index.js
+++ src/components/PostPage/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 import { MDXRemote } from 'next-mdx-remote';
 import PropTypes from 'prop-types';
 import { styled } from '@stitches';
 import MDXComponents from '@components/MDXComponents';
 
+const getSimplifiedSlug = (s) => s.replace(' ', '-');
+
 const PostHeader = styled('div', {
   marginBottom: '$4',
-  borderBottom: '1px solid $text'
+  paddingBottom: '$2',
+  borderBottom: '1px solid $text',
+  p: {
+    marginBottom: '$2'
+  }
 });
 
 const PostWrapper = styled('div', {
@@ -15,15 +22,41 @@ const PostWrapper = styled('div', {
   }
 });
 
+const TagWrapper = styled('div', {
+  display: 'flex',
+  flexWrap: 'wrap',
+  gap: '$2'
+});
+
+const Tag = styled(Link, {
+  borderRadius: '$sm',
+  padding: '$0half $2',
+  margin: 0,
+  fontSize: '$2xs',
+  background: 'rgba(0, 0, 0, 0.03)',
+  whiteSpace: 'nowrap',
+  textDecoration: 'none'
+});
+
+
 function Post({ content, node }) {
-  const { frontmatter: { title, excerpt }, meta: { date } } = node;
+  const { frontmatter: { title, excerpt, tags}, meta: { date } } = node;
 
   return (
     <PostWrapper>
       <PostHeader>
-        <h2>{title}</h2>
+        <h1>{title}</h1>
         <p>{date}</p>
         <p>{excerpt}</p>
+        {tags && (
+          <TagWrapper>
+            {tags.sort().map(tag => (
+              <Tag href={`/tags/${getSimplifiedSlug(tag)}`} passHref key={tag}>
+                {tag}
+              </Tag>
+            ))}
+          </TagWrapper>
+        )}
       </PostHeader>
       <MDXRemote
         {...content}
